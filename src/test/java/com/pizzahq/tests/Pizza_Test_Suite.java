5d7c674c5a7afd34fb39ec5b9c8ec34def7e093d
ushac5517 <ushachoudhury5517@gmail.com>
2021-09-03 15:37:48 +1000

    Updated Pizza_Test_Suite NewSideDish_Test

--- src/test/java/com/pizzahq/tests/Pizza_Test_Suite.java
+++ src/test/java/com/pizzahq/tests/Pizza_Test_Suite.java
@@ -28,8 +28,8 @@ public class Pizza_Test_Suite {
 
         // Arrange
         var homePage = new HomePage(driver);
-        homePage.content_Button_Click();
-        homePage.content_SubmitButton_Click();
+        homePage.click_Content_Button();
+        homePage.click_Content_SubmitButton();
 
         // Act
         var errorMessages =driver.findElements(By.className("form-error"));
@@ -47,38 +47,42 @@ public class Pizza_Test_Suite {
 
         // Arrange
         var homePage = new HomePage(driver);
-        homePage.content_Button_Click();
-        homePage.content_SubmitButton_Click();
 
         // Act
+        homePage.click_Content_Button();
+        homePage.click_Content_SubmitButton();
+
         driver.findElement(By.id("forename")).sendKeys("Dan");
         driver.findElement(By.id("email")).sendKeys("dan@abc.com");
         driver.findElement(By.id("message")).sendKeys("Nice Pizza");
 
         // Assert
         Assertions.assertTrue( driver.findElements(By.id("forename-err")).size() == 0);
-        Assertions.assertTrue( driver.findElements(By.id("forename-err")).size() == 0);
-        Assertions.assertTrue( driver.findElements(By.id("forename-err")).size() == 0);
+        Assertions.assertTrue( driver.findElements(By.id("email-err")).size() == 0);
+        Assertions.assertTrue( driver.findElements(By.id("message-err")).size() == 0);
 
     }
 
     @Test
     public void NewSideDish_Test(){
+
+        // Arrange
         var homePage = new HomePage(driver);
-        homePage.menu_Button_Click();
-        homePage.menu_Page_SidesButton_Click();
 
+        // Act
+        homePage.click_Menu_Button();
+        homePage.click_Menu_Page_SidesButton();
 
-        var flashcards = driver.findElements(By.className("v-card__text"));
-        for( var flashcard : flashcards ) {
-            var newFlag = flashcard.findElement(By.xpath("//*[@id=\"app\"]/div/main/div/div/div[2]/div[2]/div/div[2]/div/div[10]/div/div[1]/div/span")).getAttribute("value");
-            System.out.println(newFlag=="New");
-            if(newFlag == "NEW"){
-                Assertions.assertEquals("Koren Sticky Wings", flashcard.findElement(By.className("name")).getText());
-            }
+        var newClass = driver.findElement(By.className("new"));
+        var newFlag = newClass.findElement(By.tagName("span")).getText();
+        if(newFlag.equals("NEW")){
 
+            // Assert
+                Assertions.assertEquals("Korean Sticky Wings", newClass.findElement(By.className("name")).getText());
+                Assertions.assertEquals("Korean Sticky Wings", newClass.findElement(By.tagName("img")).getAttribute("alt"));
         }
-    }
+
+}
 
     @AfterEach
     public void CleanUp() {
--- src/test/java/com/pizzahq/ui/HomePage.java
+++ src/test/java/com/pizzahq/ui/HomePage.java
@@ -11,18 +11,18 @@ public class HomePage {
     }
 
 
-    public void content_Button_Click(){
+    public void click_Content_Button(){
         driver.findElement(By.cssSelector("[aria-label=contact]")).click();
     }
 
-    public void content_SubmitButton_Click(){
+    public void click_Content_SubmitButton(){
         driver.findElement(By.cssSelector("[aria-label=submit]")).click();
     }
 
-    public void menu_Button_Click() {
+    public void click_Menu_Button() {
         driver.findElement(By.cssSelector("[aria-label=menu]")).click();
     }
-    public void menu_Page_SidesButton_Click() {
+    public void click_Menu_Page_SidesButton() {
         driver.findElement(By.xpath("//*[@id=\"app\"]/div/main/div/div/div[2]/div[1]/div[2]/div/div[3]")).click();
     }
 
